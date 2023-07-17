4ab7392dc4cf0563984f2a8921c93f5bd827bd9f
juan cruz <jcrnietos@gmail.com>
2023-07-17 19:22:34 -0300

    f

--- src/date.js
+++ src/date.js
@@ -33,6 +33,26 @@
       src: "https://firebasestorage.googleapis.com/v0/b/imagenes-placeressintacc.appspot.com/o/y.png?alt=media&token=22660ecc-17e1-4d33-b47c-b799d9325cc6",
       alt: "proyecto7",
       desc: "https://frontend-placeressintacc.vercel.app/"
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/imagenes-placeressintacc.appspot.com/o/1.png?alt=media&token=18b5a9e0-859d-4ea4-9586-652e1b0b6ced",
+      alt: "proyecto8",
+      desc: "https://nuevo-portfolio-vite.vercel.app/"
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/imagenes-placeressintacc.appspot.com/o/2.png?alt=media&token=6c5bee0f-a2cb-4bac-a413-7c58e8ea4e27",
+      alt: "proyecto9",
+      desc: "https://nuevo-portfolio-vite.vercel.app/"
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/imagenes-placeressintacc.appspot.com/o/3.png?alt=media&token=ce0e1f34-6513-4249-9f9e-3565da763a9c",
+      alt: "proyecto10",
+      desc: "https://nuevo-portfolio-vite.vercel.app/"
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/imagenes-placeressintacc.appspot.com/o/4.png?alt=media&token=7b335106-7701-47ad-a481-6c6abc95b1a7",
+      alt: "proyecto11",
+      desc: "https://frontend-placeressintacc.vercel.app/"
     }
   ]
 
